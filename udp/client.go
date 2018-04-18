351ec132356359271107f0e26527e176abf0eb09
Amit Tewari <tewari.a@gmail.com>
2018-04-18 19:11:25 +0200

    load domain and auth DNS together

--- udp/client.go
+++ udp/client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 	"time"
 
 	"golang.org/x/net/dns/dnsmessage"
@@ -35,7 +36,7 @@ func main() {
 	go dnsWriter(Conn)
 
 	defer Conn.Close()
-	time.Sleep(time.Second * 20)
+	time.Sleep(time.Second * 700)
 }
 func dnsReader(conn *net.UDPConn) {
 	for {
@@ -51,17 +52,17 @@ func dnsReader(conn *net.UDPConn) {
 		var msg dnsmessage.Message
 		err = msg.Unpack(buf)
 		checkError(err)
-		fmt.Printf("\n"+time.Now().Format("15:04:05.000000000")+" : ")
-		if msg.Header.RCode == dnsmessage.RCodeSuccess {
+		fmt.Printf("\n" + time.Now().Format("15:04:05.000000000") + " : ")
+		if msg.Header.RCode == dnsmessage.RCodeSuccess  {
 			//fmt.Printf("%+s %+v", msg.Answers[0].Header.Name, msg.Answers[0].Body)
 			fmt.Printf("%+v", msg)
 			continue
 		} else if msg.Header.RCode == dnsmessage.RCodeNameError {
 
-			fmt.Printf("%+s : RCodeNameError", msg.Questions[0].Name)
+			fmt.Printf("%+s : RCodeNameError : ERROR-1", msg.Questions[0].Name)
 			continue
 		} else {
-			fmt.Printf("%+s : RCode = %d", msg.Questions[0].Name, msg.Header.RCode)
+			fmt.Printf("%+s : RCode = %d : ERROR-2", msg.Questions[0].Name, msg.Header.RCode)
 			continue
 		}
 		fmt.Printf("\n\nDNS Reader : %v \n%T\n%+v \n\n%s\n", msg, msg.Answers[0].Body, msg.Answers[0].Body, msg.Answers[0].Header.Name)
@@ -142,21 +143,27 @@ func dnsWriter(conn *net.UDPConn) {
 				Class: dnsmessage.ClassINET,
 			}}}
 	msg.Questions[0].Name = mustNewName("google.com.")
-	scanner := bufio.NewScanner(os.Stdin)
 	var i uint16 = 0
+
+	fileHandle, _ := os.Open("/home/amit/nuWithAuthNS.Feed.txt")
+	defer fileHandle.Close()
+	scanner := bufio.NewScanner(fileHandle)
+
 	for scanner.Scan() {
 		time.Sleep(time.Millisecond)
 		// `Text` returns the current token, here the next line,
 		// from the input.
 		i++
-		domain := scanner.Text()
-		//fmt.Printf("%s", domain)
-		msg.Header.ID = i % 65000
-		msg.Questions[0].Name = mustNewName(domain + ".")
+		//domain := scanner.Text()
+		domain := strings.Split(scanner.Text(), " ")
+		//fmt.Println(domain[0], domain[1])
+		msg.Header.ID = i%65000 + 100
+		msg.Questions[0].Name = mustNewName(domain[0])
 		buf, err := msg.Pack()
 		//fmt.Printf("DNS writer : %v %d %s\n", msg, len(buf), msg.Questions[0].Name)
 		checkError(err)
-		DNSServerAddr, err := net.ResolveUDPAddr("udp",  os.Getenv("DNS_REMOTE")+":53")
+		//DNSServerAddr, err := net.ResolveUDPAddr("udp", os.Getenv("DNS_REMOTE")+":53")
+		DNSServerAddr, err := net.ResolveUDPAddr("udp", domain[1]+":53")
 		checkError(err)
 		_, err = conn.WriteToUDP(buf, DNSServerAddr)
 		if err != nil {
@@ -165,5 +172,5 @@ func dnsWriter(conn *net.UDPConn) {
 		}
 	}
 	fmt.Print("All Read : ")
-	fmt.Printf(time.Now().Format("15:04:05.000000000")+"\n")
+	fmt.Printf(time.Now().Format("15:04:05.000000000") + "\n")
 }
